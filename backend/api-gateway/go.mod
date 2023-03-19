250c8cc522a34b9e1749d9dc624d34251158f1e0
Darek Stopka <dariusz.marek.stopka@gmail.com>
2023-03-19 16:14:43 +0100

    Init api gateway

---
+++ backend/api-gateway/go.mod
@@ -0,0 +1,13 @@
+module github.com/dstopka/notebook-app/backend/gateway
+
+go 1.20
+
+require (
+	github.com/deepmap/oapi-codegen v1.12.4
+	github.com/go-chi/chi/v5 v5.0.8
+)
+
+require (
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+)
---
+++ backend/api-gateway/internal/infrastructure/doc.go
@@ -0,0 +1,4 @@
+package infrastructure
+
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=config_types.yaml ../../../../api/openapi/api.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=config_api.yaml ../../../../api/openapi/api.yaml
